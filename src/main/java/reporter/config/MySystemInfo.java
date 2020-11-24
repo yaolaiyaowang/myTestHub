c77a0b984f503117188f64e6b70d0dd3f4cd4e69
yaolaiyaowang <33151924+yaolaiyaowang@users.noreply.github.com>
2020-11-24 12:05:04 +0800

    修改参数

--- src/main/java/reporter/config/MySystemInfo.java
+++ src/main/java/reporter/config/MySystemInfo.java
@@ -22,7 +22,7 @@ public class MySystemInfo implements SystemInfo {
         try {
             properties.load(inputStream);
             systemInfo.put("environment", properties.getProperty("Environment"));
-            systemInfo.put("测试人员", "mazy");
+            systemInfo.put("测试人员", properties.getProperty("username"));
         } catch (IOException e) {
             e.printStackTrace();
         }
